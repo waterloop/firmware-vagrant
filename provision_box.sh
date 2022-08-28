7e0ca285cbd6a162a8e40bf7ac1efe74a6b82595
Ryan Mah <rmah@uwaterloo.ca>
2022-08-27 21:33:10 -0400

    allows users to add custom configurations that aren't overwritten by `git pull`

--- provision_box.sh
+++ provision_box.sh
@@ -8,25 +8,28 @@ sudo apt update
 sudo apt update --fix-missing
 sudo apt upgrade
 
-sudo apt install -y git
-sudo apt install -y make
-sudo apt install -y build-essential
-sudo apt install -y gcc-arm-none-eabi
-sudo apt install -y gdb
-sudo apt install -y gdb-multiarch
-sudo apt install -y bear
-sudo apt install -y clangd
-sudo apt install -y stlink-tools
-sudo apt install -y python3
-sudo apt install -y python3-pip
-sudo apt install -y python3-venv
-sudo apt install -y curl
-sudo apt install -y wget
-sudo apt install -y nano
-sudo apt install -y vim
-sudo apt install -y net-tools
-sudo apt install -y can-utils
-sudo apt install -y screen
+sudo apt install -y \
+    git \
+    locales \
+    locales-all \
+    make \
+    build-essential \
+    gcc-arm-none-eabi \
+    gdb \
+    gdb-multiarch \
+    bear \
+    clangd \
+    stlink-tools \
+    python3 \
+    python3-pip \
+    python3-venv \
+    curl \
+    wget \
+    nano \
+    vim \
+    net-tools \
+    can-utils \
+    screen
 
 # install latest NodeJS
 sudo curl -sL https://deb.nodesource.com/setup_16.x -o /tmp/nodesource_setup.sh
@@ -34,12 +37,13 @@ sudo bash /tmp/nodesource_setup.sh
 sudo apt install -y nodejs
 
 # build openocd from source cus the apt version can be weirdge
-sudo apt install -y libtool
-sudo apt install -y pkg-config
-sudo apt install -y autoconf
-sudo apt install -y automake
-sudo apt install -y texinfo
-sudo apt install -y libusb-1.0-0-dev
+sudo apt install -y \
+    libtool \
+    pkg-config \
+    autoconf \
+    automake \
+    texinfo \
+    libusb-1.0-0-dev
 
 git clone https://github.com/openocd-org/openocd.git
 cd openocd
@@ -55,11 +59,12 @@ rm -rf openocd
 
 
 # install useful python libraries...
-pip3 install gdbgui
-pip3 install python-can
-pip3 install pygments
-pip3 install numpy
-pip3 install pandas
-pip3 install click
+pip3 install \
+    gdbgui \
+    python-can \
+    pygments \
+    numpy \
+    pandas \
+    click
 
 
